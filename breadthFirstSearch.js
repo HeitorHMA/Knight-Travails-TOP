c960da3c771f9cf6b289634ba2cf39bb7439b239
Ezekiel <156817493+HeitorHMA@users.noreply.github.com.>
2024-11-23 19:43:27 -0300

    create Queu objhect

--- breadthFirstSearch.js
+++ breadthFirstSearch.js
@@ -1,6 +1,6 @@
-class vertex {
-    constructor(distance = null, predecessor = null){
-        this.distance = distance
-        this.predecessor = predecessor
-    }
+const Queue = function(){
+    this.items = [];
+};
+Queue.prototype.enqueue = function(obj){
+    this.items.push(obj);
 };
