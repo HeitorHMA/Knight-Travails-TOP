1f3c193eea2558a0a26f634e3adb8762f2d49de0
Ezekiel <156817493+HeitorHMA@users.noreply.github.com.>
2024-11-24 13:25:43 -0300

    create chess square class

---
+++ knightTravails.js
@@ -0,0 +1,14 @@
+class chessSquare {
+    constructor(x,y,dist){
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+    };
+
+
+};
+function findMoves(x,y){
+    const moveX = [+1,-1,+1,-1,+2,-2,+2,-2];
+    const moveY = [+2 +2,-2,-2,+1,+1,-1,-1];
+
+}
