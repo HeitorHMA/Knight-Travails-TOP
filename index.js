fe7a41f0ea364d8eb4cfaff10d3009b5b786fdfe
Ezekiel <156817493+HeitorHMA@users.noreply.github.com.>
2024-11-24 15:48:40 -0300

    create possibleMoves function

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 function createBoard(size){
     let arr = [];
-    for(i = 0; i < size ; i++){
-        for(j = 0; j < size ; j++){
+    for(i = 1; i <= size ; i++){
+        for(j = 1; j <= size ; j++){
             arr.push([i,j])
         }
     };
+    return arr
 };
+function validMove(squareX,squareY,size){
+    if(squareX >= 1 && squareY >= 1 && squareX <= size && squareY <= size){
+        return true
+    }
+    return false
+};
+function possibleMoves(squareX,squareY){
+    const xMoves = [-2,+2,-2,+2,-1,+1,-1,+1];
+    const yMoves = [+1,+1,-1,-1,+2,+2,-2,-2];
+    let x;
+    let y;
+    let moves = [];
+    for(i=1;i<=8;i++){
+        x = squareX + xMoves[i];
+        y = squareY + yMoves[i];
+        moves.push([x,y]);
+    };
+    return moves
+}
+console.log(possibleMoves(1,2))
