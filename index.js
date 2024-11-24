03ad3f0635c8ae8308da42e5fac7a965714a9480
Ezekiel <156817493+HeitorHMA@users.noreply.github.com.>
2024-11-24 16:42:13 -0300

    create validator,possible moves and test all

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function validMove(squareX,squareY,size){
     }
     return false
 };
-function possibleMoves(squareX,squareY){
+function possibleMoves(squareX,squareY,size){
     const xMoves = [-2,+2,-2,+2,-1,+1,-1,+1];
     const yMoves = [+1,+1,-1,-1,+2,+2,-2,-2];
     let x;
@@ -22,8 +22,10 @@ function possibleMoves(squareX,squareY){
     for(i=1;i<=8;i++){
         x = squareX + xMoves[i];
         y = squareY + yMoves[i];
-        moves.push([x,y]);
+        if(validMove(x,y,size)){
+            moves.push([x,y]);
+        }
     };
     return moves
 }
-console.log(possibleMoves(1,2))
+console.log(possibleMoves(1,2,8))
